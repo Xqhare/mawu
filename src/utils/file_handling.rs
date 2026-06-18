b47275fbf0f9ad9a15845a11ca04587b463ea566
Xqhare <xqhare@web.de>
2026-06-18 16:05:31 +0200

    refactor(utils): use NemesisError for file handling operations

--- src/utils/file_handling.rs
+++ src/utils/file_handling.rs
@@ -1,18 +1,25 @@
 use std::{collections::VecDeque, fs::read_to_string, path::Path};
-
-use crate::errors::MawuError;
+use nemesis::NemesisError;
 
 /// This function reads the contents of a file, and converts the bytes from a Vec<u8> to a `VecDeque`<char>.
 /// It only accepts valid UTF-8 encoded files, returning an error otherwise.
-pub fn read_file<T: AsRef<Path>>(path: T) -> Result<VecDeque<char>, MawuError> {
-    let out = read_to_string(path.as_ref())
-        .map_err(MawuError::IoError)?
+pub fn read_file<T: AsRef<Path>>(path: T) -> Result<VecDeque<char>, NemesisError> {
+    let path_ref = path.as_ref();
+    let out = read_to_string(path_ref)
+        .map_err(|e| {
+            NemesisError::new("mawu::utils::file_handling", e)
+                .add_ctx(format!("Failed to read file at: {}", path_ref.display()))
+        })?
         .chars()
         .collect::<VecDeque<char>>();
     Ok(out)
 }
 
 /// This function writes a file with the given contents.
-pub fn write_file<T: AsRef<Path>, C: AsRef<[u8]>>(path: T, contents: C) -> Result<(), MawuError> {
-    std::fs::write(path.as_ref(), contents).map_err(MawuError::IoError)
+pub fn write_file<T: AsRef<Path>, C: AsRef<[u8]>>(path: T, contents: C) -> Result<(), NemesisError> {
+    let path_ref = path.as_ref();
+    std::fs::write(path_ref, contents).map_err(|e| {
+        NemesisError::new("mawu::utils::file_handling", e)
+            .add_ctx(format!("Failed to write file at: {}", path_ref.display()))
+    })
 }
